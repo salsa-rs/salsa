145202e37681716bc4c92345fc78b1c9f6aba189
Maxwell Elliot Heiber <mheiber@fb.com>
2021-12-30 11:01:52 +0000

    Remove ': salsa::Database' bound from two examples
    Two examples had a superfluous bound
    ': salsa::Database' that wasn't present
    in the `compiler` example.
    
    The `query_group` macro adds this bound
    automatically.
    
    This change can lead to a trailing `+` in
    the bounds list. I verified this is OK by
    running the examples and verifying that the production
    is allowed
    [per the Rust Reference](https://doc.rust-lang.org/reference/trait-bounds.html)

--- examples/compiler/interner.rs
+++ examples/compiler/interner.rs
@@ -1,7 +1,7 @@
 use crate::values::*;
 
 #[salsa::query_group(InternerDatabase)]
-pub trait Interner: salsa::Database {
+pub trait Interner {
     #[salsa::interned]
     fn intern_field(&self, field: FieldData) -> Field;
 
