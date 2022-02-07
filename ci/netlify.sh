2ff73b9ae5ac128eeeb958542fd43ccb113b55e3
Niko Matsakis <niko@alum.mit.edu>
2022-02-07 15:41:50 -0500

    trace commands

--- ci/netlify.sh
+++ ci/netlify.sh
@@ -2,6 +2,8 @@
 #
 # Script meant to be run from netlify
 
+set -x
+
 MDBOOK_VERSION='0.4.12'
 MDBOOK_LINKCHECK_VERSION='0.7.4'
 MDBOOK_MERMAID_VERSION='0.8.3'
@@ -11,3 +13,4 @@ curl -L https://github.com/badboy/mdbook-mermaid/releases/download/v$MDBOOK_MERM
 curl -L https://github.com/Michael-F-Bryan/mdbook-linkcheck/releases/download/v$MDBOOK_LINKCHECK_VERSION/mdbook-linkcheck.v$MDBOOK_LINKCHECK_VERSION.x86_64-unknown-linux-gnu.zip -O
 unzip mdbook-linkcheck.v$MDBOOK_LINKCHECK_VERSION.x86_64-unknown-linux-gnu.zip -d ~/.cargo/bin
 chmod +x ~/.cargo/bin/mdbook-linkcheck
+mdbook build
