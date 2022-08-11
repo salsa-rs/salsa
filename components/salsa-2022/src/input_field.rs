da43bd2ba6cf2c1b426475b47500263a5894871b
Mihail Mihov <mmihov.personal@gmail.com>
2022-08-11 20:24:32 +0300

    Add InputFieldIngredient

---
+++ components/salsa-2022/src/input_field.rs
@@ -0,0 +1,67 @@
+use std::hash::Hash;
+use rustc_hash::FxHashMap;
+use crate::{Durability, IngredientIndex, Revision, Runtime};
+use crate::cycle::CycleRecoveryStrategy;
+use crate::ingredient::Ingredient;
+use crate::key::DependencyIndex;
+use crate::runtime::local_state::QueryInputs;
+use crate::runtime::StampedValue;
+
+pub struct InputFieldIngredient<K, F> {
+    index: IngredientIndex,
+    map: FxHashMap<K, StampedValue<F>>
+}
+
+impl<K, F> InputFieldIngredient<K, F>
+where K: Eq + Hash
+{
+    pub fn new(index: IngredientIndex) -> Self {
+        Self {
+            index,
+            map: Default::default(),
+        }
+    }
+
+    pub fn store(
+        &mut self,
+        runtime: &mut Runtime,
+        key: K,
+        value: F,
+        durability: Durability,
+    ) -> Option<F> {
+        let revision = runtime.current_revision();
+        let stamped_value = StampedValue {
+            value,
+            durability,
+            changed_at: revision
+        };
+
+        if let Some(old_value) = self.map.insert(key, stamped_value) {
+            Some(old_value.value)
+        } else {
+            None
+        }
+    }
+
+    pub fn fetch(
+        &self,
+        key: K,
+    ) -> &F {
+        &self.map.get(&key).unwrap().value
+    }
+}
+
+impl<DB: ?Sized, K, F> Ingredient<DB> for InputFieldIngredient<K, F>
+{
+    fn cycle_recovery_strategy(&self) -> CycleRecoveryStrategy {
+        CycleRecoveryStrategy::Panic
+    }
+
+    fn maybe_changed_after(&self, _db: &DB, _input: DependencyIndex, _revision: Revision) -> bool {
+        false
+    }
+
+    fn inputs(&self, _key_index: crate::Id) -> Option<QueryInputs> {
+        None
+    }
+}
