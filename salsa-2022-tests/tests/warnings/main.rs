b14d12ada9ac840bcbbe515dca30aa74fb6d1a7e
Bernardo Uriarte <berublan@gmail.com>
2022-08-26 18:51:51 +0200

    avoid warnings in generated code

---
+++ salsa-2022-tests/tests/warnings/main.rs
@@ -0,0 +1,6 @@
+//! Test that macros don't generate code with warnings
+
+#![deny(warnings)]
+
+mod needless_borrow;
+mod unused_variable_db;
---
+++ salsa-2022-tests/tests/warnings/needless_borrow.rs
@@ -0,0 +1,19 @@
+trait Db: salsa::DbWithJar<Jar> {}
+
+#[salsa::jar(db = Db)]
+struct Jar(TokenTree);
+
+#[derive(Copy, Clone, Debug, PartialEq, Eq, Hash)]
+enum Token {}
+
+impl salsa::DebugWithDb<dyn Db + '_> for Token {
+    fn fmt(&self, _f: &mut std::fmt::Formatter<'_>, _db: &dyn Db) -> std::fmt::Result {
+        unreachable!()
+    }
+}
+
+#[salsa::tracked(jar = Jar)]
+struct TokenTree {
+    #[return_ref]
+    tokens: Vec<Token>,
+}
