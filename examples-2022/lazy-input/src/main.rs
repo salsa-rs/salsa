0403696c4e524cfa7bf5f6569e753ad8e21aa43b
Jack Rickard <jack.rickard@outlook.com>
2022-09-11 01:46:49 +0100

    Update on-demand input docs

--- examples-2022/lazy-input/src/main.rs
+++ examples-2022/lazy-input/src/main.rs
@@ -10,14 +10,22 @@ use notify_debouncer_mini::{
 };
 use salsa::DebugWithDb;
 
+// ANCHOR: main
 fn main() -> Result<()> {
+    // Create the channel to receive file change events.
     let (tx, rx) = unbounded();
     let mut db = Database::new(tx);
+
     let initial_file_path = std::env::args_os()
         .nth(1)
         .ok_or_else(|| eyre!("Usage: ./lazy-input <input-file>"))?;
+
+    // Create the initial input using the input method so that changes to it
+    // will be watched like the other files.
     let initial = db.input(initial_file_path.into())?;
     loop {
+        // Compile the code starting at the provided input, this will read other
+        // needed files using the on-demand mechanism.
         let sum = compile(&db, initial);
         let diagnostics = compile::accumulated::<Diagnostic>(&db, initial);
         if diagnostics.is_empty() {
@@ -32,20 +40,37 @@ fn main() -> Result<()> {
             eprintln!("{}", log);
         }
 
-        for event in rx.recv().unwrap().unwrap() {
-            let path = event.path.canonicalize().unwrap();
+        // Wait for file change events, the output can't change unless the
+        // inputs change.
+        for event in rx.recv()?.unwrap() {
+            let path = event.path.canonicalize().wrap_err_with(|| {
+                format!("Failed to canonicalize path {}", event.path.display())
+            })?;
             let file = match db.files.get(&path) {
                 Some(file) => *file,
                 None => continue,
             };
-            file.set_contents(&mut db)
-                .to(std::fs::read_to_string(path).unwrap());
+            // `path` has changed, so read it and update the contents to match.
+            // This creates a new revision and causes the incremental algorithm
+            // to kick in, just like any other update to a salsa input.
+            let contents = std::fs::read_to_string(path)
+                .wrap_err_with(|| format!("Failed to read file {}", event.path.display()))?;
+            file.set_contents(&mut db).to(contents);
         }
     }
 }
+// ANCHOR_END: main
 
 #[salsa::jar(db = Db)]
 struct Jar(Diagnostic, File, ParsedFile, compile, parse, sum);
+
+// ANCHOR: db
+#[salsa::input]
+struct File {
+    path: PathBuf,
+    #[return_ref]
+    contents: String,
+}
 
 trait Db: salsa::DbWithJar<Jar> {
     fn input(&self, path: PathBuf) -> Result<File>;
@@ -71,26 +96,19 @@ impl Database {
     }
 }
 
-impl salsa::Database for Database {
-    fn salsa_event(&self, event: salsa::Event) {
-        // don't log boring events
-        if let salsa::EventKind::WillExecute { .. } = event.kind {
-            self.logs
-                .lock()
-                .unwrap()
-                .push(format!("{:?}", event.debug(self)));
-        }
-    }
-}
-
 impl Db for Database {
     fn input(&self, path: PathBuf) -> Result<File> {
         let path = path
             .canonicalize()
             .wrap_err_with(|| format!("Failed to read {}", path.display()))?;
         Ok(match self.files.entry(path.clone()) {
+            // If the file already exists in our cache then just return it.
             Entry::Occupied(entry) => *entry.get(),
+            // If we haven't read this file yet set up the watch, read the
+            // contents, store it in the cache, and return it.
             Entry::Vacant(entry) => {
+                // Set up the watch before reading the contents to try to avoid
+                // race conditions.
                 let watcher = &mut *self.file_watcher.lock().unwrap();
                 watcher
                     .watcher()
@@ -101,6 +119,19 @@ impl Db for Database {
                 *entry.insert(File::new(self, path, contents))
             }
         })
+    }
+}
+// ANCHOR_END: db
+
+impl salsa::Database for Database {
+    fn salsa_event(&self, event: salsa::Event) {
+        // don't log boring events
+        if let salsa::EventKind::WillExecute { .. } = event.kind {
+            self.logs
+                .lock()
+                .unwrap()
+                .push(format!("{:?}", event.debug(self)));
+        }
     }
 }
 
@@ -121,13 +152,6 @@ impl Diagnostic {
             ),
         )
     }
-}
-
-#[salsa::input]
-struct File {
-    path: PathBuf,
-    #[return_ref]
-    contents: String,
 }
 
 #[salsa::tracked]
