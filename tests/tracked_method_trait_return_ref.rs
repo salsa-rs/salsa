6e22d6ec422dc2eba0e027804da89575bf5aa7cc
Niko Matsakis <niko@alum.mit.edu>
2024-07-18 06:33:21 -0400

    wip

---
+++ tests/tracked_method_trait_return_ref.rs
@@ -0,0 +1,45 @@
+use salsa::Database as _;
+
+#[salsa::input]
+struct Input {
+    number: usize,
+}
+
+trait Trait {
+    fn test(self, db: &dyn salsa::Database) -> &Vec<String>;
+}
+
+#[salsa::tracked]
+impl Trait for Input {
+    #[salsa::tracked(return_ref)]
+    fn test(self, db: &dyn salsa::Database) -> Vec<String> {
+        (0..self.number(db))
+            .map(|i| format!("test {}", i))
+            .collect()
+    }
+}
+
+#[salsa::db]
+#[derive(Default)]
+struct Database {
+    storage: salsa::Storage<Self>,
+}
+
+#[salsa::db]
+impl salsa::Database for Database {}
+
+#[test]
+fn invoke() {
+    Database::default().attach(|db| {
+        let input = Input::new(db, 3);
+        let x: &Vec<String> = input.test(db);
+        expect_test::expect![[r#"
+            [
+                "test 0",
+                "test 1",
+                "test 2",
+            ]
+        "#]]
+        .assert_debug_eq(x);
+    })
+}
