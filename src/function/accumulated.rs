beedbd18e50cc18068a83ef0cac403918399b14b
Phoebe Szmucer <phoebe.szmucer@gmail.com>
2024-07-21 18:18:02 +0100

    Refactor

--- src/function/accumulated.rs
+++ src/function/accumulated.rs
@@ -1,6 +1,6 @@
-use std::collections::HashSet;
+use std::collections::VecDeque;
 
-use crate::{accumulator, storage::DatabaseGen, DatabaseKeyIndex, Id};
+use crate::{accumulator, hash::FxHashSet, storage::DatabaseGen, DatabaseKeyIndex, Id};
 
 use super::{Configuration, IngredientImpl};
 
@@ -23,42 +23,26 @@ where
         // First ensure the result is up to date
         self.fetch(db, key);
 
-        // Recursively accumulate outputs from children
-        self.database_key_index(key).traverse_children::<C, _>(
-            db,
-            &mut |query| accumulator.produced_by(runtime, query, &mut output),
-            &mut HashSet::new(),
-        );
+        let db_key = self.database_key_index(key);
+        let mut visited: FxHashSet<DatabaseKeyIndex> = std::iter::once(db_key).collect();
+        let mut stack = VecDeque::new();
+        stack.push_front(db_key);
 
-        output
-    }
-}
+        while let Some(k) = stack.pop_front() {
+            accumulator.produced_by(runtime, k, &mut output);
 
-impl DatabaseKeyIndex {
-    pub fn traverse_children<C, F>(
-        &self,
-        db: &C::DbView,
-        handler: &mut F,
-        visited: &mut HashSet<DatabaseKeyIndex>,
-    ) where
-        C: Configuration,
-        F: (FnMut(DatabaseKeyIndex)),
-    {
-        handler(*self);
-        visited.insert(*self);
+            let origin = db.lookup_ingredient(k.ingredient_index).origin(k.key_index);
+            let inputs = origin.iter().flat_map(|origin| origin.inputs());
 
-        let origin = db
-            .lookup_ingredient(self.ingredient_index)
-            .origin(self.key_index);
-
-        if let Some(origin) = origin {
-            for input in origin.inputs() {
-                if let Ok(input) = TryInto::<DatabaseKeyIndex>::try_into(input) {
-                    if !visited.contains(&input) {
-                        input.traverse_children::<C, F>(db, handler, visited);
+            for input in inputs {
+                if let Ok(input) = input.try_into() {
+                    if visited.insert(input) {
+                        stack.push_back(input);
                     }
                 }
             }
         }
+
+        output
     }
 }
