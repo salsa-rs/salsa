ac474a9c8df57f81ac63d7515c3150ea758f5255
Niko Matsakis <niko@alum.mit.edu>
2024-07-24 09:53:24 +0000

    restore parallel_cycle_mid_recover

--- tests/parallel/main.rs
+++ tests/parallel/main.rs
@@ -1,9 +1,7 @@
 mod setup;
 
 mod parallel_cycle_all_recover;
-#[cfg(disabled)]
 mod parallel_cycle_mid_recover;
-#[cfg(disabled)]
 mod parallel_cycle_none_recover;
 #[cfg(disabled)]
 mod parallel_cycle_one_recover;
