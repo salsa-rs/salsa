4609fd00433593dcb843ec2a7b479e72ef3b168d
Micha Reiser <micha@reiser.io>
2024-08-20 21:34:38 +0200

    Add failing test

--- tests/tracked_fn_constant.rs
+++ tests/tracked_fn_constant.rs
@@ -2,8 +2,17 @@
 //! compiles and executes successfully.
 #![allow(warnings)]
 
+use crate::common::LogDatabase;
+
+mod common;
+
 #[salsa::tracked]
 fn tracked_fn(db: &dyn salsa::Database) -> u32 {
+    44
+}
+
+#[salsa::tracked]
+fn tracked_custom_db(db: &dyn LogDatabase) -> u32 {
     44
 }
 
@@ -11,4 +20,10 @@ fn tracked_fn(db: &dyn salsa::Database) -> u32 {
 fn execute() {
     let mut db = salsa::DatabaseImpl::new();
     assert_eq!(tracked_fn(&db), 44);
+}
+
+#[test]
+fn execute_custom() {
+    let mut db = common::LoggerDatabase::new();
+    assert_eq!(tracked_custom_db(&db), 44);
 }
