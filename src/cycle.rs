e5d9b4ff638b519aaf2f1556f988c78c6f6594c3
Carl Meyer <carl@oddbird.net>
2025-03-03 18:04:39 -0800

    add module doc comment in cycle.rs about cycle handling

--- src/cycle.rs
+++ src/cycle.rs
@@ -1,3 +1,49 @@
+//! Cycle handling
+//!
+//! Salsa's default cycle handling is quite simple: if we encounter a cycle (that is, if we attempt
+//! to execute a query that is already on the active query stack), we panic.
+//!
+//! By setting `cycle_fn` and `cycle_initial` arguments to `salsa::tracked`, queries can opt-in to
+//! fixed-point iteration instead.
+//!
+//! We call the query which triggers the cycle (that is, the query that is already on the stack
+//! when it is called again) the "cycle head". The cycle head is responsible for managing iteration
+//! of the cycle. When a cycle is encountered, if the cycle head has `cycle_fn` and `cycle_initial`
+//! set, it will call the `cycle_initial` function to generate an "empty" or "initial" value for
+//! fixed-point iteration, which will be returned to its caller. Then each query in the cycle will
+//! compute a value normally, but every computed value will track the head(s) of the cycles it is
+//! part of. Every query's "cycle heads" are the union of all the cycle heads of all the queries it
+//! depends on. A memoized query result with cycle heads is called a "provisional value".
+//!
+//! For example, if `qa` calls `qb`, and `qb` calls `qc`, and `qc` calls `qa`, then `qa` will call
+//! its `cycle_initial` function to get an initial value, and return that as its result to `qc`,
+//! marked with `qa` as cycle head. `qc` will compute its own provisional result based on that, and
+//! return to `qb` a result also marked with `qa` as cycle head. `qb` will similarly compute and
+//! return a provisional value back to `qa`.
+//!
+//! When a query observes that it has just computed a result which contains itself as a cycle head,
+//! it recognizes that it is responsible for resolving this cycle and calls its `cycle_fn` to
+//! decide how to do so. The `cycle_fn` function is passed the provisional value just computed for
+//! that query and the count of iterations so far, and must return either
+//! `CycleRecoveryAction::Iterate` (which signals that the cycle head should re-iterate the cycle),
+//! or `CycleRecoveryAction::Fallback` (which signals that the cycle head should replace its
+//! computed value with the given fallback value).
+//!
+//! If the cycle head ever observes that the provisional value it just recomputed is the same as
+//! the provisional value from the previous iteration, the cycle has converged. The cycle head will
+//! mark that value as final (by removing itself as cycle head) and return it.
+//!
+//! Other queries in the cycle will still have provisional values recorded, but those values should
+//! now also be considered final! We don't eagerly walk the entire cycle to mark them final.
+//! Instead, we wait until the next time that provisional value is read, and then we check if all
+//! of its cycle heads have a final result, in which case it, too, can be marked final. (This is
+//! implemented in `shallow_verify_memo` and `validate_provisional`.)
+//!
+//! If the `cycle_fn` returns a fallback value, the cycle head will replace its provisional value
+//! with that fallback, and then iterate the cycle one more time. A fallback value is expected to
+//! result in a stable, converged cycle. If it does not (that is, if the result of another
+//! iteration of the cycle is not the same as the fallback value), we'll panic.
+
 use crate::key::DatabaseKeyIndex;
 use rustc_hash::FxHashSet;
 
