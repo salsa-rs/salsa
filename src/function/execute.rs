2d4c9a4ff11e731d00f8b1baa2b986bdaccf0897
Micha Reiser <micha@reiser.io>
2025-04-25 12:11:53 +0200

    Try

--- src/function/execute.rs
+++ src/function/execute.rs
@@ -60,7 +60,7 @@ where
                 // (and are owned by the query) alive even if the query in this iteration no longer creates them.
                 // The query not re-creating the tracked struct doesn't guarantee that there
                 // aren't any other queries depending on it.
-                if old_memo.verified_at.load() == revision_now && old_memo.may_be_provisional() {
+                if old_memo.may_be_provisional() && old_memo.verified_at.load() == revision_now {
                     active_query.seed_outputs(old_memo.revisions.origin.outputs());
                 }
             }
