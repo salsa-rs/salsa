08e42195a28a6f18bb9d4e7852a27e89909cd0d8
Lukas Wirth <lukastw97@gmail.com>
2025-05-19 12:28:47 +0200

    Implement an `!Update` bound escape hatch for tracked fn

---
+++ tests/tracked_fn_orphan_escape_hatch.rs
@@ -0,0 +1,25 @@
+//! Test that a `tracked` fn on a `salsa::input`
+//! compiles and executes successfully.
+#![allow(warnings)]
+
+use std::marker::PhantomData;
+
+#[salsa::input]
+struct MyInput {
+    field: u32,
+}
+
+#[derive(Debug, Clone, Copy, PartialEq, Eq)]
+struct NotUpdate<'a>(PhantomData<fn() -> &'a ()>);
+
+#[salsa::tracked(unsafe(non_update_return_type))]
+fn tracked_fn(db: &dyn salsa::Database, input: MyInput) -> NotUpdate<'_> {
+    NotUpdate(PhantomData)
+}
+
+#[test]
+fn execute() {
+    let mut db = salsa::DatabaseImpl::new();
+    let input = MyInput::new(&db, 22);
+    tracked_fn(&db, input);
+}
