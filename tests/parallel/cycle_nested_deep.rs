f573178ab83944db76de867df3e1423da7c26623
Micha Reiser <micha@reiser.io>
2025-05-25 14:14:45 +0200

    Add runaway query repro

---
+++ tests/parallel/cycle_nested_deep.rs
@@ -0,0 +1,120 @@
+//! Test a nested-cycle scenario across three threads:
+//!
+//! ```text
+//! Thread T1          Thread T2         Thread T3
+//! ---------          ---------         ---------
+//!    |                  |                  |
+//!    v                  |                  |
+//! query_a()             |                  |
+//!  ^  |                 v                  |
+//!  |  +------------> query_b()             |
+//!  |                  ^   |                v
+//!  |                  |   +------------> query_c()
+//!  |                  |                    |
+//!  +------------------+--------------------+
+//!
+//! ```
+use crate::sync::thread;
+use crate::{Knobs, KnobsDatabase};
+
+use salsa::CycleRecoveryAction;
+
+#[derive(Debug, PartialEq, Eq, PartialOrd, Ord, Hash, Clone, Copy, salsa::Update)]
+struct CycleValue(u32);
+
+const MIN: CycleValue = CycleValue(0);
+const MAX: CycleValue = CycleValue(3);
+
+// Signal 1: T1 has entered `query_a`
+// Signal 2: T2 has entered `query_b`
+// Signal 3: T3 has entered `query_c`
+
+#[salsa::tracked(cycle_fn=cycle_fn, cycle_initial=initial)]
+fn query_a(db: &dyn KnobsDatabase) -> CycleValue {
+    query_b(db)
+}
+
+#[salsa::tracked(cycle_fn=cycle_fn, cycle_initial=initial)]
+fn query_b(db: &dyn KnobsDatabase) -> CycleValue {
+    let c_value = query_c(db);
+    CycleValue(c_value.0 + 1).min(MAX)
+}
+
+#[salsa::tracked(cycle_fn=cycle_fn, cycle_initial=initial)]
+fn query_c(db: &dyn KnobsDatabase) -> CycleValue {
+    let d_value = query_d(db);
+    let e_value = query_e(db);
+    let b_value = query_b(db);
+    let a_value = query_a(db);
+
+    CycleValue(d_value.0.max(e_value.0).max(b_value.0).max(a_value.0))
+}
+
+#[salsa::tracked(cycle_fn=cycle_fn, cycle_initial=initial)]
+fn query_d(db: &dyn KnobsDatabase) -> CycleValue {
+    query_c(db)
+}
+
+#[salsa::tracked]
+fn query_e(db: &dyn KnobsDatabase) -> CycleValue {
+    query_c(db)
+}
+
+fn cycle_fn(
+    _db: &dyn KnobsDatabase,
+    _value: &CycleValue,
+    _count: u32,
+) -> CycleRecoveryAction<CycleValue> {
+    CycleRecoveryAction::Iterate
+}
+
+fn initial(_db: &dyn KnobsDatabase) -> CycleValue {
+    MIN
+}
+
+#[test_log::test]
+fn the_test() {
+    shuttle::replay(
+        || {
+            let db_t1 = Knobs::default();
+            let db_t2 = db_t1.clone();
+            let db_t3 = db_t1.clone();
+
+            let t1 = thread::spawn(move || query_a(&db_t1));
+            let t2 = thread::spawn(move || query_d(&db_t2));
+            let t3 = thread::spawn(move || query_e(&db_t3));
+
+            let r_t1 = t1.join().unwrap();
+            let r_t2 = t2.join().unwrap();
+            let r_t3 = t3.join().unwrap();
+
+            assert_eq!((r_t1, r_t2, r_t3), (MAX, MAX, MAX));
+        },
+        "
+    9102b312f9d1f2f7a3aeeef5ee01000000000000802449922449922449922449922449922449
+    9224499224499224499224499224499224499224499224499224499224499224499224499224
+    4992244992244992244992244992244992244992244992244992244992244992244992244992
+    2449922449922449922449922449922449922449922449922449922449922449922449922449
+    9224499224499224499224499224499224499224499224499224499224499224499224499224
+    4992244992244992244992244992244992244992244992244992244992244992244992244992
+    2449922449922449922449922449922449922449922449922449922449922449922449922449
+    9224499224499224499224499224499224499224499224499224499224499224499224499224
+    4992244992244992244992244992244992244992244992244992244992244992244992244992
+    2449922449922449922449922449922449922449922449922449922449922449922449922449
+    9224499224499224499224499224499224499224499224499224499224499224499224499224
+    4992244992244992244992244992244992244992244992244992244992244992244992244992
+    2449922449922449922449922449922449922449922449922449922449922449922449922449
+    9224499224499224499224499224499224499224499224499224499224499224499224499224
+    4992244992244992244992244992244992244992244992244992244992244992244992244992
+    2449922449922449922449922449922449922449922449922449922449922449922449922449
+    9224499224499224499224499224499224499224499224499224499224499224499224499224
+    4992244992244992244992244992244992244992244992244992244992244992244992244992
+    2449922449922449922449922449922449922449922449922449922449922449922449922449
+    9224499224499224499224499224499224499224499224499224499224499224499224499224
+    4992244992244992244992244992244992244992244992244992244992244992244992244992
+    2449922449922449922449922449922449922449922449922449922449922449922449922449
+    9224499224499224499224499224499224499224499224499224499224812449922449922449
+    922449922461dbb66ddbb66ddbb66ddbb66ddbb66ddb3600
+    ",
+    );
+}
