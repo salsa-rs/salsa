2a54667121dc0a700f62d9049ff1d14a0c7fa722
Chayim Refael Friedman <chayimfr@gmail.com>
2025-05-26 07:50:43 +0300

    Allow lifetimes in arguments in tracked fns with >1 parameters (#880)

---
+++ tests/tracked_fn_interned_lifetime.rs
@@ -0,0 +1,14 @@
+#[salsa::interned]
+struct Interned<'db> {
+    field: i32,
+}
+
+#[salsa::tracked]
+fn foo<'a>(_db: &'a dyn salsa::Database, _: Interned<'_>, _: Interned<'a>) {}
+
+#[test]
+fn the_test() {
+    let db = salsa::DatabaseImpl::new();
+    let i = Interned::new(&db, 123);
+    foo(&db, i, i);
+}
