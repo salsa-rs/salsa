e257df12eabd566825ba53bb12d782560b9a4dcd
Chayim Refael Friedman <chayimfr@gmail.com>
2025-09-18 11:57:22 +0300

    Provide a method to attach a database even if it's different from the current attached one (#992)
    * Provide a method to attach a database even if it's different from the current attached one
    
    rust-analyzer needs this.
    
    * Fix Clippy on beta
    
    * Update compile fail test outputs
    
    ---------
    
    Co-authored-by: Lukas Wirth <lukas@zed.dev>

--- src/lib.rs
+++ src/lib.rs
@@ -67,7 +67,7 @@ pub use self::runtime::Runtime;
 pub use self::storage::{Storage, StorageHandle};
 pub use self::update::Update;
 pub use self::zalsa::IngredientIndex;
-pub use crate::attach::{attach, with_attached_database};
+pub use crate::attach::{attach, attach_allow_change, with_attached_database};
 
 pub mod prelude {
     #[cfg(feature = "accumulator")]
