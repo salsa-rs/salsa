6ef7ef2e6fbcf0982083c67657b0e1e777149db1
Micha Reiser <micha@reiser.io>
2025-10-31 15:51:46 -0400

    Remove 1.90 constraint from compile_fail tests

--- tests/compile_fail.rs
+++ tests/compile_fail.rs
@@ -1,6 +1,6 @@
 #![cfg(all(feature = "inventory", feature = "persistence"))]
 
-#[rustversion::all(stable, since(1.90))]
+#[rustversion::all(stable)]
 #[test]
 fn compile_fail() {
     let t = trybuild::TestCases::new();
