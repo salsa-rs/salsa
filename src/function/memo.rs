5d70dbe50b677ec70ca2027263ac15549c7df67c
Shunsuke Shibayama <sbym1346@gmail.com>
2025-11-21 04:51:02 +0900

    Update memo.rs

--- src/function/memo.rs
+++ src/function/memo.rs
@@ -562,7 +562,7 @@ mod _memory_usage {
 
         fn recover_from_cycle<'db>(
             _: &'db Self::DbView,
-            _: &crate::Cycle<'_>,
+            _: &crate::Cycle,
             _: &Self::Output<'db>,
             value: Self::Output<'db>,
             _: Self::Input<'db>,
