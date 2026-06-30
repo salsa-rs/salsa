148e91419509785202e589f6c74dea5c875abd9b
Micha Reiser <micha@reiser.io>
2026-06-30 14:11:33 +0200

    test: cover non-SalsaValue tracked function results

---
+++ tests/compile-fail/tracked_fn_return_not_salsa_value.rs
@@ -0,0 +1,16 @@
+use std::marker::PhantomData;
+
+use salsa::Database as Db;
+
+#[salsa::input]
+struct MyInput {}
+
+#[derive(Clone, Debug, PartialEq, Eq)]
+struct NotSalsaValue<'db>(PhantomData<fn() -> &'db ()>);
+
+#[salsa::tracked]
+fn tracked_fn<'db>(_db: &'db dyn Db, _input: MyInput) -> NotSalsaValue<'db> {
+    NotSalsaValue(PhantomData)
+}
+
+fn main() {}
