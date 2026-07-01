d1b092ee9d8de7a86197ab94e083dba9354bb13a
Micha Reiser <micha@reiser.io>
2026-07-01 10:41:04 +0200

    chore: isolate CodSpeed comparison benchmarks (#1222)
    * chore: isolate CodSpeed comparison benchmarks
    
    * chore: inline comparison benchmark support
    
    * chore: simplify benchmark executables
    
    * chore: use default benchmark names

---
+++ benches/compare/new_input.rs
@@ -0,0 +1,37 @@
+use std::hint::black_box;
+
+fn main() {
+    divan::main();
+}
+
+#[divan::bench]
+fn new_input(bencher: divan::Bencher) {
+    bencher
+        .with_inputs(|| {
+            let db = salsa::DatabaseImpl::default();
+
+            // Prepopulate ingredients.
+            let input = Input::new(black_box(&db), black_box("hello, world!".to_owned()));
+            let len = length(black_box(&db), black_box(input));
+            assert_eq!(black_box(len), 13);
+
+            db
+        })
+        .bench_local_refs(|db| {
+            let input = Input::new(black_box(db), black_box("hello, world!".to_owned()));
+            let len = length(black_box(db), black_box(input));
+            assert_eq!(black_box(len), 13);
+        });
+}
+
+#[salsa::input]
+pub struct Input {
+    #[returns(ref)]
+    pub text: String,
+}
+
+#[salsa::tracked(returns(copy))]
+#[inline(never)]
+pub fn length(db: &dyn salsa::Database, input: Input) -> usize {
+    input.text(db).len()
+}
